829fd16a6100d1f6c0cdc2f904e8d96f91d8ed68
juliano <julianojd@ymail.com>
2019-09-18 00:39:51 -0300

    Added ArticleViewModel

---
+++ goodnews/ViewModels/ArticleViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  ArticleViewModel.swift
+//  goodnews
+//
+//  Created by juliano jose dziadzio on 18/09/19.
+//  Copyright © 2019 juliano jose dziadzio. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleListViewModel {
+    let articles: [Article]
+}
+
+
+extension ArticleListViewModel {
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func numberOfRowsInsections(_ section: Int ) -> Int {
+        return self.articles.count
+    }
+    
+    func articleAtIndex(_ index: Int) -> ArticleViewModel {
+        let article = self.articles[index]
+        return ArticleViewModel(article)
+    }
+}
+
+
+
+struct ArticleViewModel {
+    private let article: Article
+}
+
+extension ArticleViewModel {
+    init(_ article: Article) {
+        self.article = article
+    }
+}
+
+
+extension ArticleViewModel {
+    var title: String {
+        return self.article.Title
+    }
+    
+    var description: String {
+        return self.article.Description
+    }
+}
+
